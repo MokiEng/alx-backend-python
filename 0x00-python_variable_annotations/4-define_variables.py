a8d667f54c1415114ebb2b9787839fe3f2175981
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-05 08:08:15 -0700

    Update 4-define_variables.py

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""Define and annotate the following variables
-with the specified values:
+"""A module that define and annotate the following variables
+with the specified values
 """
 
 
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str = “Holberton”
+school: str = "Holberton"
