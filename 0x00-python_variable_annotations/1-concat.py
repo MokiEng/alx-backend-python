33903dc4ed699888c54d2e052b2d827168bbb018
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-05 07:26:27 -0700

    Update 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' a type-annotated function that concatenate two strings.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' a function returns a concatenated string
+    '''
+    return str1 + str2
