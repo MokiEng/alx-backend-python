eff02bf769515a1647d7e819e5d0bc783ba0749e
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-05 07:36:11 -0700

    To string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" a module that changes to string
+representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """ a function that returns a string representation of
+    the float.
+    """
+    return str(n)
