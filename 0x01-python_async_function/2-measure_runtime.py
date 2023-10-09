8cc3d93c5b75092f8c12521b86dad38d16b21342
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-09 08:39:01 -0700

    Update 2-measure_runtime.py

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """time module to measure an approximate elapsed time."""
-from asyncio import run
-from time import time
+import asyncio
+import time
+
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     Measures the total execution time for wait_n(n, max_delay)
     and returns total_time / n.
