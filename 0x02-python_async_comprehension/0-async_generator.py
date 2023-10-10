1ca83d0328bd81740b9aeddbfbb52dfd58fad493
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-10 12:19:47 -0700

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,11 +2,12 @@
 """ a module that defines async_generator"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """a coroutine called async_generator that takes no arguments.
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10)
