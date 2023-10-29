b95539ff1ddeb2ae59fbe290b866dfa99b562c1f
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-29 06:02:18 -0700

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -44,18 +44,17 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-    def test_get_json(
-            self,
-            test_url: str,
-            test_payload: Dict,
-            ) -> None:
+    def test_get_json(self, test_url, test_payload):
         """
         Create a Mock object to simulate the response from requests.get.
         """
-        attrs = {'json.return_value': test_payload}
-        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
-            self.assertEqual(get_json(test_url), test_payload)
-            req_get.assert_called_once_with(test_url)
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+
+        with patch('requests.get', return_value=mock_response) as mock_get:
+            result = get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
 
 
 class TestMemoize(unittest.TestCase):
