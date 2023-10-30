494d6353c0ee113c03b018ee4d2cd816f0cfb3c6
MokiEng <meconen20@gmail.com>
2023-10-30 06:17:56 +0000

    task6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -32,6 +32,20 @@ class TestGithubOrgClient(unittest.TestCase):
         expected_result = ["repo1", "repo2", "repo3"]
         self.assertEqual(public_repos_url, expected_result)
 
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """Define the known payload."""
+        payload = [{'name': 'repo1'}, {'name': 'repo2'}, {'name': 'repo3'}]
+        with patch(
+                'client.GithubOrgClient._public_repos_url',
+                return_value="https://api.github.com/orgs/testorg/repos"):
+            mock_get_json.return_value = payload
+            client = GithubOrgClient("testorg")
+            repos = client.public_repos()
+            expected_result = ["repo1", "repo2", "repo3"]
+            self.assertEqual(repos, expected_result)
+        mock_get_json.assert_called_once()
+
 
 if __name__ == '__main__':
     unittest.main()
